7bbcb69d3af81352cf09d74a54c8b72ed91d150e
Sebastien Jodogne <s.jodogne@gmail.com>
2020-08-04 14:15:51 +0200

    fix

--- ViewerPlugin/Plugin.cpp
+++ ViewerPlugin/Plugin.cpp
@@ -312,7 +312,7 @@ extern "C"
       return -1;
     }
 
-#if defined(ORTHANC_FRAMEWORK_VERSION_IS_ABOVE)  // This indicates Orthanc framework >= 1.7.2
+#if ORTHANC_FRAMEWORK_VERSION_IS_ABOVE(1, 7, 2)
     Orthanc::Logging::InitializePluginContext(context);
 #else
     Orthanc::Logging::Initialize(context);
